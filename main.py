52e952b22104c3630107dff4ea40cfdab7bcad16
BinDruid <abharya.dev@gmail.com>
2023-02-10 20:39:20 +0330

    refactor: add command line option to get table name

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
-import os
-import psycopg2
-import psycopg2.extras
-from src.utils.cursor import MigrationCursor, MigrationConnection
+import sys
+from src.migrator import Migrator
 
-postgres_connection = MigrationConnection(os.environ["DB_URL"])
-postgres_cursor: MigrationCursor = postgres_connection.cursor()
 
-postgres_cursor.table_name = "dashboard_train"
-postgres_cursor.set_validation_schema()
-postgres_cursor.get_table()
-for row in postgres_cursor:
-    try:
-        postgres_cursor.migrate_new_record(row, {"id": "int"})
+def main():
+    table_name = sys.argv[1]
+    migrator = Migrator(table_name)
+    migrator.migrate()
 
-    except psycopg2.Error as exp:
-        print(exp.pgerror)
-        break
 
-postgres_connection.commit()
-postgres_connection.close()
+if __name__ == "__main__":
+    main()
