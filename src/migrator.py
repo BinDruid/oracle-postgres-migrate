062ac6f3c586082fc2bd363ea5c80238830b5f28
BinDruid <abharya.dev@gmail.com>
2023-02-15 19:25:56 +0330

    refactor: enable bulk insert for postgres

--- src/migrator.py
+++ src/migrator.py
@@ -1,8 +1,7 @@
-import os
-import time
 import psycopg2
 import cx_Oracle
 import psycopg2.extras
+from psycopg2.extras import execute_values
 from src.utils.logging import Logger
 from src.cursors.psycopg_extensions import PostgresConnection, PostgresCursor
 from src.cursors.cx_oracle_extensions import OracleConnection, OracleCursor
@@ -11,38 +10,56 @@ from src.cursors.cx_oracle_extensions import OracleConnection, OracleCursor
 class Migrator:
     def __init__(self, table_name):
         self.table_name = table_name
-        self.oracle_connection = OracleConnection()
+        self.oracle_connection = OracleConnection.new_connection()
         self.oracle_cursor: OracleCursor = self.oracle_connection.cursor()
         self.oracle_cursor.table_name = table_name
-        self.postgres_connection = PostgresConnection(os.environ["POSTGRES_URL"])
+        self.postgres_connection = PostgresConnection.new_connection()
         self.postgres_cursor: PostgresCursor = self.postgres_connection.cursor()
         self.postgres_cursor.table_name = table_name
+        self.total_records = self.oracle_cursor.get_total_records()
+        (
+            self.columns,
+            self.values,
+            self.oracle_column_types,
+        ) = self.oracle_cursor.get_table_meta()
+        self.insert_query = (
+            f"insert into {self.table_name} ({self.columns}) values({self.values})"
+        )
+        self.insert_query = f"insert into {self.table_name} ({self.columns}) values %s"
+        self.insert_bulk_size = self.oracle_cursor.arraysize
         self.logger = Logger()
 
-    def migrate(self):
-        total_records = self.oracle_cursor.get_total_records()
-        columns, values, oracle_column_types = self.oracle_cursor.get_table_meta()
+    def _config_initial_setting(self):
         self.oracle_cursor.set_row_factory()
         self.postgres_cursor.set_validation_schema()
+
+    def migrate(self):
+        self._config_initial_setting()
+        self.postgres_cursor.disable_foreign_key()
+        values_list = []
         current_row = 1
         for row in self.oracle_cursor:
             try:
-                insert_query = (
-                    f"insert into {self.table_name} ({columns}) values({values});"
-                )
-                self.postgres_cursor.validate_new_record(row, oracle_column_types)
-                values_list = tuple(
-                    [
-                        self.oracle_cursor.read_from_LOB(column)
-                        for column in row.values()
-                    ]
-                )
-                self.postgres_cursor.execute(insert_query, values_list)
+                self.postgres_cursor.validate_new_record(row, self.oracle_column_types)
+                values_list.append(self.oracle_cursor.read_values(row))
                 self.logger.progress(
                     current_row,
-                    total_records,
-                    suffix=f"Migrated {current_row} records out of {total_records}",
+                    self.total_records,
+                    suffix=f"Migrated {current_row} records out of {self.total_records}",
                 )
+
+                if (
+                    current_row % self.insert_bulk_size == 0
+                    or current_row == self.total_records
+                ):
+                    execute_values(
+                        self.postgres_cursor,
+                        self.insert_query,
+                        values_list,
+                    )
+
+                    values_list.clear()
+
                 current_row += 1
 
             except psycopg2.Error as exp:
@@ -50,9 +67,15 @@ class Migrator:
                 self.logger.write_error(row)
                 break
 
+            except Exception as exp:
+                self.logger.write_error(f"\n{exp}")
+                self.logger.write_error(row)
+                break
+
         self.logger.write_warning(f"\nCommitting to database.")
+        self.postgres_connection.commit()
+        self.postgres_cursor.enable_foreign_key()
         self.postgres_connection.commit()
         self.postgres_cursor.close()
         self.oracle_cursor.close()
         self.logger.write_success(f"\nSuccessfully populated {self.table_name}.")
-        # TODO add time elapsed
